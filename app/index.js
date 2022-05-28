dce9d50f326ebc88e55c4e49afe20b7b7de9907a
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-05-28 09:17:32 +0430

    Implement server.js and start App

--- app/index.js
+++ app/index.js
@@ -1,1 +1,20 @@
 'use strict'
+
+const express = require('express')
+const app = express()
+
+
+// middleware
+app.use(express.json())
+
+
+
+module.exports = async (port) => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 'use strict'
+
+require('dotenv').config()
+const startApp = require('./app')
+const port = process.env.APP_PORT || 5000
+
+// start application
+startApp(port)
