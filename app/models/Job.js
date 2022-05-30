44e8a72243bdd83eaa429796df690744d743a9c3
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-05-30 15:55:49 +0430

    Implement Job model

--- app/models/Job.js
+++ app/models/Job.js
@@ -1,2 +1,31 @@
 'use strict'
 
+const mongoose = require('mongoose')
+
+const jobSchema = new mongoose.Schema({
+    company: {
+        type: String,
+        required: [true, 'Please provide company name'],
+        maxlength: 100
+    },
+    position: {
+        type: String,
+        required: [true, 'Please provide position'],
+        maxlength: 150
+    },
+    status: {
+        type: String,
+        enum: ['interview', 'declined', 'pending'],
+        default: 'pending'
+    },
+    createBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please provide user']
+    }
+}, {
+    timestamps: true
+})
+
+
+module.exports = mongoose.model('Job', jobSchema)
